c0c4e193e4632cff2fae67813820c9cdc49db3ca
gonzalo <gonzalocasal@gmail.com>
2018-03-01 21:36:10 -0300

    data sql

--- src/main/resources/sql/ciudad.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `ciudad` (`ID_CIUDAD`, `NOMBRE`) VALUES
-(1, 'La Plata'),
-(2, 'Ensenada'),
-(3, 'Berisso');
--- src/main/resources/sql/peluquero.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `peluquero` (`ID_CIUDAD`, `NOMBRE`) VALUES
-(1, 'Jorge'),
-(2, 'Raul'),
-(3, 'Romina');
